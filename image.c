c7ca547b55e98914264351502e581fe6ae17ae15
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-09-11 08:46:33 +0300

    Fix in tabbed with alpha patch (#3)
    * Fix in tabbed with alpha patch
    
    Co-authored-by: Jared Forrest <jared_forrest@mailbox.org>

--- image.c
+++ image.c
@@ -482,7 +482,7 @@ void img_render(img_t *img)
 		if ((bg = imlib_create_image(dw, dh)) == NULL)
 			error(EXIT_FAILURE, ENOMEM, NULL);
 		imlib_context_set_image(bg);
-		imlib_image_set_has_alpha(0);
+		imlib_image_set_has_alpha(1);
 
 		if (img->alpha) {
 			int i, c, r;
@@ -510,6 +510,7 @@ void img_render(img_t *img)
 		imlib_free_image();
 		imlib_context_set_color_modifier(img->cmod);
 	} else {
+		imlib_image_set_has_alpha(1);
 		imlib_render_image_part_on_drawable_at_size(sx, sy, sw, sh, dx, dy, dw, dh);
 	}
 	img->dirty = false;
