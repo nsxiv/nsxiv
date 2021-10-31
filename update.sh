fee6a2d4a0b919819e6f78db4e5649b608a78932
Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
2021-10-31 16:56:54 -0300

    optimize update script

--- update.sh
+++ update.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-git show "master:nsxiv.1" > nsxiv.1
-
-groff -mandoc -Thtml < nsxiv.1 > index.html
+git show "master:nsxiv.1" | groff -mandoc -Thtml > index.html
 awk '/<style type="text\/css">/,/<\/style>/ { if ( $0 ~ /<\/style>/ ) print "<link rel=\"stylesheet\" href=\"index.css\">"; next } 1' \
     index.html > index.html.tmp && mv index.html.tmp index.html
-
-rm nsxiv.1
