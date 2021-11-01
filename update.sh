bbc0abc64570fc62d47e473445624fad5110d667
Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
2021-11-01 00:47:31 -0300

    clean up update.sh script
    Co-authored-by: N-R-K <79544946+N-R-K@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+css='<link rel="stylesheet" href="index.css">'
+icon_src='https://raw.githubusercontent.com/nsxiv/nsxiv/master/icon/16x16.png'
+icon='<link rel="icon" type="image/png" href="'"$icon_src"'" sizes="16x16">'
+
 git show "master:nsxiv.1" | groff -mandoc -Thtml |
-  sed 's|^</head>|<link rel="stylesheet" href="index.css"><link rel="icon" type="image/png" href="https://raw.githubusercontent.com/nsxiv/nsxiv/master/icon/16x16.png" sizes="16x16"></head>|' > index.html
+  sed 's|^</head>|'"${css}${icon}"'</head>|' > index.html
