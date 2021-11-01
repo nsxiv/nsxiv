256e6e16bef53570c3d7964864b50ea36886d4ed
eylles <ed.ylles1997@gmail.com>
2021-11-01 13:27:35 -0600

    online manual script and style (#151)
    * create script to generate the updated html from nsxiv.1
    * add style.css to improve readability and look
    
    Co-authored-by: Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
    Co-authored-by: N-R-K <79544946+N-R-K@users.noreply.github.com>

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+css='<link rel="stylesheet" href="style.css">'
+icon_src='https://raw.githubusercontent.com/nsxiv/nsxiv/master/icon/16x16.png'
+icon='<link rel="icon" type="image/png" href="'"$icon_src"'" sizes="16x16">'
+
+git show "master:nsxiv.1" | groff -mandoc -Thtml |
+  sed 's|^</head>|'"${css}${icon}"'</head>|' > index.html
