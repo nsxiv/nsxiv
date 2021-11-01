1c598dfa1659b330e581314b20926dea25eef0f4
Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
2021-10-31 23:40:56 -0300

    change update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-git show "master:nsxiv.1" | groff -mandoc -Thtml > index.html
-awk '/<style type="text\/css">/,/<\/style>/ { if ( $0 ~ /<\/style>/ ) print "<link rel=\"stylesheet\" href=\"index.css\">"; next } 1' \
-    index.html > index.html.tmp && mv index.html.tmp index.html
+git show "master:nsxiv.1" | groff -mandoc -Thtml |
+  sed 's|^</head>|<link rel="stylesheet" href="index.css"></head>|' > index.html
