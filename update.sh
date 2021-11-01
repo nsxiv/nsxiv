887f3bb52b3da27c47a91271ad8e1149cebe6b81
Guilherme Rugai Freire <41879254+GRFreire@users.noreply.github.com>
2021-10-31 23:55:38 -0300

    add favicon

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 git show "master:nsxiv.1" | groff -mandoc -Thtml |
-  sed 's|^</head>|<link rel="stylesheet" href="index.css"></head>|' > index.html
+  sed 's|^</head>|<link rel="stylesheet" href="index.css"><link rel="icon" type="image/png" href="https://raw.githubusercontent.com/GRFreire/nsxiv/master/icon/16x16.png" sizes="16x16"></head>|' > index.html
