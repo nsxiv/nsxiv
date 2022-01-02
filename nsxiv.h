f7145db7f8a5d136a522e0045e5ff39b2279e04c
N-R-K <79544946+N-R-K@users.noreply.github.com>
2022-01-03 02:13:23 +0600

    remove unused function and typedef (#199)
    byteorder_t and size_readable is not used anywhere within the code.
    
    byteorder_t seems to be a remain from some time sxiv handled exif data itself instead of relying on a library, introduced in 691c6d7, and probably became irrelevant when libexif was added as dependency again. And size_readable from some time it displayed the file size in the window title, introduced in bad9a70.

--- nsxiv.h
+++ nsxiv.h
@@ -56,11 +56,6 @@
 }
 
 typedef enum {
-	BO_BIG_ENDIAN,
-	BO_LITTLE_ENDIAN
-} byteorder_t;
-
-typedef enum {
 	MODE_ALL,
 	MODE_IMAGE,
 	MODE_THUMB
@@ -352,7 +347,6 @@ void* emalloc(size_t);
 void* erealloc(void*, size_t);
 char* estrdup(const char*);
 void error(int, int, const char*, ...);
-void size_readable(float*, const char**);
 int r_opendir(r_dir_t*, const char*, bool);
 int r_closedir(r_dir_t*);
 char* r_readdir(r_dir_t*, bool);
