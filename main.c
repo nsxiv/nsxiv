nsxiv/nsxiv

commit 6922d5d01b03b0f584a411803d2da45b07f5af01

replace select() with poll() (#270)
usage of select (3) in modern programs is typically discouraged.
this simply replaces the select call with poll (3) instead.

and since poll conveniently ignores negative fds, this also reduces
needs for some special casing.

this also handles error if they occur, while old implementation didn't.
other than the error handling, no change in functionality should occur.

main.c
@@ -29,7 +29,7 @@
 #include <errno.h>
 #include <locale.h>
 #include <signal.h>
-#include <sys/select.h>
+#include <poll.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <time.h>
@@ -701,9 +701,9 @@ static void on_buttonpress(const XButtonEvent *bev)
 
 static void run(void)
 {
-	int xfd;
-	fd_set fds;
-	struct timeval timeout;
+	enum { FD_X, FD_INFO, FD_ARL, FD_CNT };
+	struct pollfd pfd[FD_CNT];
+	struct timeval timeout = {0};
 	const struct timespec ten_ms = {0, 10000000};
 	bool discard, init_thumb, load_thumb, to_set;
 	XEvent ev, nextev;
@@ -730,21 +730,16 @@ static void run(void)
 				if (!tns_load(&tns, tns.initnext, false, true))
 					remove_file(tns.initnext, false);
 			} else {
-				xfd = ConnectionNumber(win.env.dpy);
-				FD_ZERO(&fds);
-				FD_SET(xfd, &fds);
-				if (info.fd != -1) {
-					FD_SET(info.fd, &fds);
-					xfd = MAX(xfd, info.fd);
-				}
-				if (arl.fd != -1) {
-					FD_SET(arl.fd, &fds);
-					xfd = MAX(xfd, arl.fd);
-				}
-				select(xfd + 1, &fds, 0, 0, to_set ? &timeout : NULL);
-				if (info.fd != -1 && FD_ISSET(info.fd, &fds))
+				pfd[FD_X].fd = ConnectionNumber(win.env.dpy);
+				pfd[FD_INFO].fd = info.fd;
+				pfd[FD_ARL].fd = arl.fd;
+				pfd[FD_X].events = pfd[FD_INFO].events = pfd[FD_ARL].events = POLLIN;
+
+				if (poll(pfd, ARRLEN(pfd), to_set ? TV_TO_MS(&timeout) : -1) < 0)
+					continue;
+				if (pfd[FD_INFO].revents & POLLIN)
 					read_info();
-				if (arl.fd != -1 && FD_ISSET(arl.fd, &fds)) {
+				if (pfd[FD_ARL].revents & POLLIN) {
 					if (arl_handle(&arl)) {
 						/* when too fast, imlib2 can't load the image */
 						nanosleep(&ten_ms, NULL);

nsxiv.h
@@ -44,6 +44,7 @@
 
 #define TV_DIFF(t1,t2) (((t1)->tv_sec  - (t2)->tv_sec ) * 1000 + \
                         ((t1)->tv_usec - (t2)->tv_usec) / 1000)
+#define TV_TO_MS(tv)   (((tv)->tv_sec * 1000) + ((tv)->tv_usec / 1000))
 
 #define TV_SET_MSEC(tv,t) {             \
   (tv)->tv_sec  = (t) / 1000;           \

commit c00803c5d99b0d95b11b10f5411acc071576e73b

fix: broken statusbar after key-handler cancellation
to reproduce:
1. have an image-info script
2. invoke the key-handler
3. cancle invocation by pressing `escape`
at this point, the statusbar ends up being empty.

the regression seems to be caused by 6922d5d (changing select to poll),
unsure why that is.

in any case, this simplifies read_info quite a bit and solves the
regression as well. in short:

* read straight into the statusbar buffer
* if read succeeds, make sure buffer is null terminated and replace any
  newline with space
* close the script

main.c
@@ -73,7 +73,6 @@ static bool resized = false;
 static struct {
 	extcmd_t f, ft;
 	int fd;
-	unsigned int i, lastsep;
 	pid_t pid;
 } info;
 
@@ -296,7 +295,6 @@ void open_info(void)
 	if (pfd.readfd >= 0) {
 		fcntl(pfd.readfd, F_SETFL, O_NONBLOCK);
 		info.fd = pfd.readfd;
-		info.i = info.lastsep = 0;
 		info.pid = pfd.pid;
 	}
 }
@@ -304,32 +302,15 @@ void open_info(void)
 static void read_info(void)
 {
 	ssize_t i, n;
-	char buf[BAR_L_LEN];
 
-	while (true) {
-		n = read(info.fd, buf, sizeof(buf));
-		if (n < 0 && errno == EAGAIN)
-			return;
-		else if (n == 0)
-			goto end;
-		for (i = 0; i < n; i++) {
-			if (buf[i] == '\n') {
-				if (info.lastsep == 0) {
-					win.bar.l.buf[info.i++] = ' ';
-					info.lastsep = 1;
-				}
-			} else {
-				win.bar.l.buf[info.i++] = buf[i];
-				info.lastsep = 0;
-			}
-			if (info.i + 1 == win.bar.l.size)
-				goto end;
+	if ((n = read(info.fd, win.bar.l.buf, win.bar.l.size - 1)) > 0) {
+		win.bar.l.buf[n] = '\0';
+		for (i = 0; i < n; ++i) {
+			if (win.bar.l.buf[i] == '\n')
+				win.bar.l.buf[i] = ' ';
 		}
+		win_draw(&win);
 	}
-end:
-	info.i -= info.lastsep;
-	win.bar.l.buf[info.i] = '\0';
-	win_draw(&win);
 	close_info();
 }
 